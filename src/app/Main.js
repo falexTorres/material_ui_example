0769db8a23767d591d9a67577842e9aa05c021ae
Alex Torres <atorres@fastyeti.com>
2016-09-21 12:36:26 -0500

    initial commit

--- src/app/Main.js
+++ src/app/Main.js
@@ -5,10 +5,11 @@
 import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
-import {deepOrange500} from 'material-ui/styles/colors';
+import {deepOrange500, purple900, purple700, grey400} from 'material-ui/styles/colors';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAppBar from './MyAppBar';
 
 const styles = {
   container: {
@@ -20,6 +21,9 @@ const styles = {
 const muiTheme = getMuiTheme({
   palette: {
     accent1Color: deepOrange500,
+    primary1Color: purple700,
+    primary2Color: purple900,
+    primary3Color: grey400,
   },
 });
 
@@ -57,25 +61,30 @@ class Main extends Component {
     );
 
     return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
-          <RaisedButton
-            label="Super Secret Password"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
-        </div>
-      </MuiThemeProvider>
+      <div className="full-width">
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <MyAppBar/>
+        </MuiThemeProvider>
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <div style={styles.container}>
+            <Dialog
+              open={this.state.open}
+              title="Super Secret Password"
+              actions={standardActions}
+              onRequestClose={this.handleRequestClose}
+            >
+              1-2-3-4-5
+            </Dialog>
+            <h1>Material-UI</h1>
+            <h2>example project</h2>
+            <RaisedButton
+              label="Super Secret Password"
+              secondary={true}
+              onTouchTap={this.handleTouchTap}
+            />
+          </div>
+        </MuiThemeProvider>
+      </div>
     );
   }
 }
